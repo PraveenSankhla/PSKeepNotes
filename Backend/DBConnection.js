4fc1a9293ad0ac8d94f3bd6121cf9d469ef40856
Praveen Sankhla <60568878+PraveenSankhla@users.noreply.github.com>
2024-01-10 05:08:41 +0530

    Update DBConnection.js

--- Backend/DBConnection.js
+++ Backend/DBConnection.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-//const constring = "mongodb+srv://pk54451636465:Since-2016@praveen.5jvvqgm.mongodb.net/psdata?retryWrites=true&w=majority"
-// mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2
 const MongoDb = async () => {
     mongoose.connect("mongodb://127.0.0.1:27017/PSKeep?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2")
     .then(() => {console.log("Connected")})
